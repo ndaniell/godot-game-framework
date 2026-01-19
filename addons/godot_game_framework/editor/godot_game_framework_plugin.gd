06d001d8f26e1e68d0ba3c26292a2fe94d1f4527
Nicholas Daniell <nick.daniell@gmail.com>
2026-01-19 17:19:42 -0500

    Update plugin configuration to simplify script path

--- addons/godot_game_framework/editor/godot_game_framework_plugin.gd
+++ addons/godot_game_framework/editor/godot_game_framework_plugin.gd
@@ -2,31 +2,25 @@
 extends EditorPlugin
 
 const AUTOLOAD_NAME := "GGF"
-const AUTOLOAD_PATH := "res://addons/godot_game_framework/GGF.gd"
 
 
 func _enter_tree() -> void:
-	# Install a single autoload singleton that bootstraps all managers.
-	# If the host project already has an autoload named GGF, do not override it.
-	if _has_autoload(AUTOLOAD_NAME):
-		push_warning(
-			"Godot Game Framework: autoload '%s' already exists; not overriding." % AUTOLOAD_NAME
+	# This editor plugin does NOT install or modify autoloads.
+	# The framework must be loaded via a project autoload named `GGF`.
+	if not _has_autoload(AUTOLOAD_NAME):
+		push_error(
+			"Godot Game Framework: missing required autoload '%s'. "
+			+ "Add an autoload named '%s' pointing to your bootstrapper (e.g. `res://src/ExampleGGF.gd`)."
+			% [AUTOLOAD_NAME, AUTOLOAD_NAME]
 		)
 		return
-	add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
 
 
 func _exit_tree() -> void:
-	# Only remove the autoload if it still points at our path.
-	# This avoids deleting a user-defined autoload with the same name.
-	if not _has_autoload(AUTOLOAD_NAME):
-		return
-	var setting_key := "autoload/%s" % AUTOLOAD_NAME
-	var val: Variant = ProjectSettings.get_setting(setting_key, null)
-	if val is String and (val as String).ends_with(AUTOLOAD_PATH):
-		remove_autoload_singleton(AUTOLOAD_NAME)
+	# Intentionally do nothing: we never create/remove autoloads.
+	return
 
 
-func _has_autoload(name: String) -> bool:
-	var setting_key := "autoload/%s" % name
+func _has_autoload(autoload_name: String) -> bool:
+	var setting_key := "autoload/%s" % autoload_name
 	return ProjectSettings.has_setting(setting_key)
